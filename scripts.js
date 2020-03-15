0f349a50e6f0709f52ec613e8e1ec5422c0b8952
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-03-15 15:37:58 -0500

    display message on button click

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
     // create first button
     let btnText = 'click me';
-    $('<button>' + btnText + '</button>').insertAfter('#1');
+    $('<button id="firstBtn">' + btnText + '</button>').insertAfter('#1');
+    
+    // display alert on button click
+    $('#firstBtn').click(function() {
+        let quoteArray = [
+            '\"Whenever I\'m about to do something, I think, \'Would an idiot do that?\'\" - Dwight Schrute',
+            '\"How would I describe myself? Three words: hardworking, alpha male, jackhammer, merciless, insatiable.\" - Dwight Schrute',
+            '\"Learn your rules, you better learn your rules, if you don\'t, you\'ll be eaten in your sleep.\" - Dwight Schrute',
+            '\"I am ready to face any challenge that might be foolish enough to face me.\" - Dwight Schrute'
+        ]
+        let randomQuote = quoteArray[Math.floor(Math.random() * quoteArray.length)];
+        alert(randomQuote);
+    })
 });
